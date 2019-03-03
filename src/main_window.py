331e5c4e01a61e027983ad8f2b77c69a2a036ab7
Stu Smith <stusmith1977@gmail.com>
2019-03-03 14:58:17 +0000

    Basic layout.

--- src/main_window.py
+++ src/main_window.py
@@ -1,29 +1,55 @@
 from tkinter import *
 
+
 class MainWindow(Frame):
 
-	def __init__(self, master=None):
-		Frame.__init__(self, master)
-		self._master = master
-		self.init_window()
+    def __init__(self, master):
+        Frame.__init__(self, master)
+        self._master = master
+        self.init_window()
 
-	def init_window(self):
-		self._master.title('blender-evolution')
+    def init_window(self):
+        self._master.title('blender-evolution')
 
-		self.init_menus()
-		self.init_grid()
+        self.grid(row=0, column=0, sticky="nsew")
+        self._master.grid_rowconfigure(0, weight=1)
+        self._master.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
 
-	def init_menus(self):
-		menubar = Menu(self._master)
-		self._master.config(menu=menubar)
+        self.init_menus()
+        self.init_grid()
 
-		file_menu = Menu(menubar)
-		file_menu.add_command(label='Exit', command=self.client_exit)
+    def init_menus(self):
+        menubar = Menu(self._master)
+        self._master.config(menu=menubar)
 
-		menubar.add_cascade(label='File', menu=file_menu)
+        file_menu = Menu(menubar)
+        file_menu.add_command(label='Exit', command=self.client_exit)
 
-	def init_grid(self):
-		Label(self._master, text='First').grid(row=0)
+        menubar.add_cascade(label='File', menu=file_menu)
 
-	def client_exit(self):
-		exit()
+    def init_grid(self):
+        grid = Frame(self)
+
+        grid.grid(row=0, column=0, sticky="nsew")
+
+        for row in range(0, 3):
+            grid.grid_rowconfigure(row, weight=1)
+        for col in range(0, 3):
+            grid.grid_columnconfigure(col, weight=1)
+
+        Label(grid, text='00').grid(row=0, column=0)
+        Label(grid, text='01').grid(row=0, column=1)
+        Label(grid, text='02').grid(row=0, column=2)
+
+        Label(grid, text='10').grid(row=1, column=0)
+        Label(grid, text='11').grid(row=1, column=1)
+        Label(grid, text='12').grid(row=1, column=2)
+
+        Label(grid, text='20').grid(row=2, column=0)
+        Label(grid, text='21').grid(row=2, column=1)
+        Label(grid, text='22').grid(row=2, column=2)
+
+    def client_exit(self):
+        exit()
